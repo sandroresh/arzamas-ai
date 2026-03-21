5401a72210d9443a33c9c8459f56a9e15fee6773
sandroresh <43700874+sandroresh@users.noreply.github.com>
2026-03-21 13:54:25 +0300

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ st.title("Управление школой 'Arzamas'")
 try:
     api_key = st.secrets["GOOGLE_API_KEY"]
     genai.configure(api_key=api_key)
-    model = genai.GenerativeModel('gemini-1.5-flash') 
+    model = genai.GenerativeModel('gemini-2.5-flash') 
 except Exception as e:
     st.error("Ошибка API Ключа Google. Проверьте 'Secrets'.")
     st.stop()
